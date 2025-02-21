ff2fbe40d88414dc7ee15de49c9ea4f2e0753761
Nicholas Stull <Nicholasp.stull@gmail.com>
2025-02-21 14:58:52 -0500

    changed favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,13 +7,22 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Contact Nick",
   description: "Contact Nick for whatever you need to contact Nick about.",
+  icons: {
+    // You can also do an array of objects if you want multiple icon types
+    icon: [
+      {
+        url: "https://media.tinkernerd.dev/favicon.svg",
+        type: "image/svg+xml",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
